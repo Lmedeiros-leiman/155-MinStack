32e8dc47f1733ebf5b66fc278e5a510b08e3af0a
Lmedeiros-leiman <lmedeiros045@gmail.com>
2023-10-13 01:00:32 -0300

    Adicionado resposta exercicio

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿
+/*
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+Implement the MinStack class:
+
+    MinStack() initializes the stack object.
+    void push(int val) pushes the element val onto the stack.
+    void pop() removes the element on the top of the stack.
+    int top() gets the top element of the stack.
+    int getMin() retrieves the minimum element in the stack.
+
+You must implement a solution with O(1) time complexity for each function.
+
+*/
+
+
+
+public class MinStack {
+
+    private Stack<int> stack;
+    private Stack<int> minStack;
+
+    public MinStack() {
+        stack = new Stack<int>();
+        minStack = new Stack<int>();
+    }
+    
+    public void Push(int val) {
+        stack.Push(val);
+        int min = Math.Min(val, minStack.Count != 0 ? minStack.Peek() : val);
+        minStack.Push(min);
+
+
+    }
+    
+    public void Pop() {
+        stack.Pop();
+        minStack.Pop();
+    }
+    
+    public int Top() {
+        return stack.Peek();
+    }
+    
+    public int GetMin() {
+        return minStack.Peek();
+        
+    }
+}
+
